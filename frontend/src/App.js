41e5ffafc61fc1e3c53e9794d44dd3df87d5c7d9
hilmawan <hilmawanhafidz4@gmail.com>
2020-04-30 17:28:57 +0700

    Header

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,15 +2,16 @@ import React from 'react';
 import './App.css';
 import { Switch, Route } from 'react-router-dom';
 import Homepage from './components/homepage'
+import Header from './components/Header'
 
 function App() {
   return (
     <div>
-      <div>Header</div>
+      <Header />
       {/* <div style={{color:'black', fontSize:'42'}}>asd</div> */}
-        <Switch>
-          <Route path={'/'} exact component={Homepage}/>
-        </Switch>
+      <Switch>
+        <Route path={'/'} exact component={Homepage} />
+      </Switch>
     </div>
   );
 }
