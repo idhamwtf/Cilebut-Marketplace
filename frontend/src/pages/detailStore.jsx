a5c673c9582aded530691ba00d491b432a197173
fakhranheit <fakhranwahyu@gmail.com>
2020-05-01 14:18:18 +0700

    b

--- frontend/src/pages/detailStore.jsx
+++ frontend/src/pages/detailStore.jsx
@@ -4,8 +4,8 @@ import { Carousel } from 'react-responsive-carousel'
 function DetailStore() {
     return (
         <div style={{ paddingTop: '50px', paddingLeft: '100px', paddingRight: '100px' }}>
-            <div style={{ border: '1px solid black', padding: '30px', display: 'flex' }}>
-                <div style={{ width: '650px', border: '1px solid black' }}>
+            <div style={{ border: '3px solid black', padding: '30px', display: 'flex', minWidth: '80vh' }}>
+                <div style={{ border: '3px solid black', width: '600px' }}>
                     <Carousel infiniteLoop useKeyboardArrows autoPlay stopOnHover>
                         <div>
                             <img src="https://wallpaperaccess.com/full/17444.jpg" />
@@ -18,8 +18,13 @@ function DetailStore() {
                         </div>
                     </Carousel>
                 </div>
-                <div style={{ border: '1px solid black', minWidth: '120vh', marginLeft: '30px' }} >
+                <div style={{ border: '3px solid black', minWidth: '110vh', marginLeft: '50px', padding: '25px' }} >
                     <h1 style={{ display: 'flex', justifyContent: 'center' }}>Playstation 2</h1>
+                    <div style={{ border: '3px solid black', minHeight: '40vh', padding: '20px', fontSize: '21px', marginLeft: '30px' }}>
+                        <div>Store Name : Adidaya Gaming </div>
+                        <div>Total Buyer : 10</div>
+                        <div>Stock : 7 </div>
+                    </div>
                 </div>
             </div>
         </div>
