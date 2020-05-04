9ecceeee2bd1df344762ae04a96ebb534df20afa
idhamwtf <idham.1422@gmail.com>
2020-05-04 15:42:02 +0700

    homepage

---
+++ frontend/src/components/jumbtoronhomepage1.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './../css/jumbtoronhomepage1.css'
+
+
+export default function Jumbotron1(){
+    return(
+        <div className='jumbotron-1'>
+            <div>Jumbotron 1</div>
+        </div>
+    )
+}
