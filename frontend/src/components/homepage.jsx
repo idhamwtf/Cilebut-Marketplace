5e8b024ef992c52734733029135d5a8e002c6af8
idhamwtf <idham.1422@gmail.com>
2020-05-05 18:21:40 +0700

    homepage jumbotron1

--- frontend/src/components/homepage.jsx
+++ frontend/src/components/homepage.jsx
@@ -2,9 +2,21 @@ import React from 'react';
 import Jumbotron1 from './../components/jumbtoronhomepage1'
 
 function Homepage() {
+
     return (
-        <div>
-            <Jumbotron1/>
+
+        <div className='homepage-box' 
+        style={{width:'1530px',
+         height:'2500px',
+         backgroundImage:`url(${'https://cf.shopee.co.id/file/5df8a66220df2ae81a60829043f23a81'})`, 
+         backgroundRepeat:'no-repeat',
+         backgroundSize:'100%',
+         backgroundColor:'#f5f5f5',
+         margin:'0px',
+         border:'1px transparent solid'}}>
+
+                <Jumbotron1/>
+
         </div>
     )
 }
