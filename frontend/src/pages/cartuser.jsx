ec28098a21395816a6b501a81a5bd808a43eb8d5
fakhranheit <fakhranwahyu@gmail.com>
2020-05-07 10:13:10 +0700

    update

--- frontend/src/pages/cartuser.jsx
+++ frontend/src/pages/cartuser.jsx
@@ -1,17 +1,18 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import '../css/cart.css'
 
 const Cart = () => {
     return (
-        <div style={{ border: '1px solid black' }}>
-            <div style={{ borderRight: '2px solid black', height: '60px', minWidth: '180px', marginLeft: '100px', padding: '10px', display: "flex" }}>
-                <Link style={{ display: 'flex', textDecoration: 'none' }} to='/'>
+        <div style={{ border: '1px solid black', display: 'flex', justifyContent: 'center' }}>
+            <div style={{ border: '2px solid black', height: 'fit-content', minWidth: '180px', padding: '10px', display: "flex" }}>
+                <Link style={{ display: 'flex', textDecoration: 'none', padding: '10px', border: '2px solid tomato' }} to='/'>
                     <img height="35px" width="135px" src="https://www.pinclipart.com/picdir/big/337-3370099_marketplace-partnership-shopee-logo-clipart.png" alt="a" />
-                    <div style={{ color: '#f63', fontSize: "26px", marginLeft: '8px' }}> |   Keranjang Belanja</div>
+                    <div style={{ color: '#f63', fontSize: "23px", marginLeft: '10px' }}> |   Keranjang Belanja</div>
                 </Link>
-                <div style={{ marginTop: '6px' }}>
-                    <input style={{ width: '450px', marginLeft: '200px' }} type="text" placeholder="Elektronik diskon s/d 90%" />
-                    <button style={{ backgroundColor: '#f63', color: 'white' }}>search</button>
+                <div style={{ border: '1px solid #f63', height: '60px' }} >
+                    <input style={{ width: '450px', marginLeft: '220px' }} type="text" placeholder="Elektronik diskon s/d 90%" />
+                    <button style={{ backgroundColor: '#f63', color: 'white', height: '41px' }} type='submit'>search</button>
                 </div>
             </div>
         </div>
