bd27431e98549dc4ffac9a25428429b3e02952ad
idhamwtf <idham.1422@gmail.com>
2020-05-08 19:00:06 +0700

    jumbotron 2 & homepage responsive

---
+++ frontend/src/components/jumbotronhomepage2.jsx
@@ -0,0 +1,180 @@
+import React from 'react';
+import './../css/jumbotronhomepage2.css'
+
+
+
+export default function Jumbotron2(){
+    return (
+        <div>
+            <div className='jumbotron2-content-1'>
+                <div className='jumbotron2-content-1-box-1'>KATEGORI</div>
+            </div>
+            <div className='jumbotron2-content-2'>
+                <div className='d-flex flex-row'>
+                    <div className='jumbotron2-content-2-box1'>
+                        <div className='jumbotron2-content-2-box1-logo'>
+                            <img src="https://cf.shopee.co.id/file/ec5c71e18beb2d6950fe691da42082d7_tn" alt="" width='100%' height='100%'/>
+                        </div>
+                        <div className='jumbotron2-content-2-box1-title'>
+                            Pakaian Pria
+                        </div>
+                    </div>
+                    <div className='jumbotron2-content-2-box1'>
+                        <div className='jumbotron2-content-2-box1-logo'>
+                            <img src="https://cf.shopee.co.id/file/0bfdaa2d899e4d16ac0e31fc17a4d6ef_tn" alt="" width='100%' height='100%'/>
+                        </div>
+                        <div className='jumbotron2-content-2-box1-title'>
+                            Handphone & Aksesoris
+                        </div>
+                    </div>
+                    <div className='jumbotron2-content-2-box1'>
+                        <div className='jumbotron2-content-2-box1-logo'>
+                            <img src="https://cf.shopee.co.id/file/1eb9ba3754c3cfebf2155bd9a9c7385d_tn" alt="" width='100%' height='100%'/>
+                        </div>
+                        <div className='jumbotron2-content-2-box1-title'>
+                            Fashion Bayi & Anak
+                        </div>
+                    </div>
+                    <div className='jumbotron2-content-2-box1'>
+                        <div className='jumbotron2-content-2-box1-logo'>
+                            <img src="https://cf.shopee.co.id/file/e7df9dc29e88bd525eab10f2a34b6e83_tn" alt="" width='100%' height='100%'/>
+                        </div>
+                        <div className='jumbotron2-content-2-box1-title'>
+                            Sepatu Wanita
+                        </div>
+                    </div>
+                    <div className='jumbotron2-content-2-box1'>
+                        <div className='jumbotron2-content-2-box1-logo'>
+                            <img src="https://cf.shopee.co.id/file/09a946c456690e19e0ea28fb0ec8215f_tn" alt="" width='100%' height='100%'/>
+                        </div>
+                        <div className='jumbotron2-content-2-box1-title'>
+                            Tas Wanita
+                        </div>
+                    </div>
+                    <div className='jumbotron2-content-2-box1'>
+                        <div className='jumbotron2-content-2-box1-logo'>
+                            <img src="https://cf.shopee.co.id/file/111b6a0545c8f85f27ac1d9e916e97bf_tn" alt="" width='100%' height='100%'/>
+                        </div>
+                        <div className='jumbotron2-content-2-box1-title'>
+                            Elektronik
+                        </div>
+                    </div>
+                    <div className='jumbotron2-content-2-box1'>
+                        <div className='jumbotron2-content-2-box1-logo'>
+                            <img src="https://cf.shopee.co.id/file/6f9965db3a657f2408ef4663c3954943_tn" alt="" width='100%' height='100%'/>
+                        </div>
+                        <div className='jumbotron2-content-2-box1-title'>
+                            Hobi & Koleksi
+                        </div>
+                    </div>
+                    <div className='jumbotron2-content-2-box1'>
+                        <div className='jumbotron2-content-2-box1-logo'>
+                            <img src="https://cf.shopee.co.id/file/be7c2970b4844433672823d31166b6b8_tn" alt="" width='100%' height='100%'/>
+                        </div>
+                        <div className='jumbotron2-content-2-box1-title'>
+                            Olahraga & Outdoor
+                        </div>
+                    </div>
+                    <div className='jumbotron2-content-2-box1'>
+                        <div className='jumbotron2-content-2-box1-logo'>
+                            <img src="https://cf.shopee.co.id/file/289b738f77694893dc1e971b42643aee_tn" alt="" width='100%' height='100%'/>
+                        </div>
+                        <div className='jumbotron2-content-2-box1-title'>
+                            Buku & Alat Tulis
+                        </div>
+                    </div>
+                    <div className='jumbotron2-content-2-box1'>
+                        <div className='jumbotron2-content-2-box1-logo'>
+                            <img src="https://cf.shopee.co.id/file/c10dbb27e7bbfd3fabac07b73900b23c_tn" alt="" width='100%' height='100%'/>
+                        </div>
+                        <div className='jumbotron2-content-2-box1-title'>
+                            Serba Serbi
+                        </div>
+                    </div>
+                </div>
+                <div className='d-flex flex-row'> 
+                    <div className='jumbotron2-content-2-box2'>
+                        <div className='jumbotron2-content-2-box2-logo'>
+                            <img src="https://cf.shopee.co.id/file/c099daa851a7c80a14eea3b66968a3f5_tn" alt="" width='100%' height='100%'/>
+                        </div>
+                        <div className='jumbotron2-content-2-box2-title'>
+                            Pakaian Wanita
+                        </div>
+                    </div>
+                    <div className='jumbotron2-content-2-box2'>
+                        <div className='jumbotron2-content-2-box2-logo'>
+                            <img src="https://cf.shopee.co.id/file/39762ff427021a9904a672a357897f1b_tn" alt="" width='100%' height='100%'/>
+                        </div>
+                        <div className='jumbotron2-content-2-box2-title'>
+                            Komputer & Aksesoris
+                        </div>
+                    </div>
+                    <div className='jumbotron2-content-2-box2'>
+                        <div className='jumbotron2-content-2-box2-logo'>
+                            <img src="https://cf.shopee.co.id/file/6afd8f9ce7b884ef3c8172445dae9de4_tn" alt="" width='100%' height='100%'/>
+                        </div>
+                        <div className='jumbotron2-content-2-box2-title'>
+                            Sepatu Pria
+                        </div>
+                    </div>
+                    <div className='jumbotron2-content-2-box2'>
+                        <div className='jumbotron2-content-2-box2-logo'>
+                            <img src="https://cf.shopee.co.id/file/99b46a30d9d4499ef992d55e10586586_tn" alt="" width='100%' height='100%'/>
+                        </div>
+                        <div className='jumbotron2-content-2-box2-title'>
+                            Tas Pria
+                        </div>
+                    </div>
+                    <div className='jumbotron2-content-2-box2'>
+                        <div className='jumbotron2-content-2-box2-logo'>
+                            <img src="https://cf.shopee.co.id/file/7d6d5cda649d5d0faa0b82776c1c178d_tn" alt="" width='100%' height='100%'/>
+                        </div>
+                        <div className='jumbotron2-content-2-box2-title'>
+                            Jam Tangan
+                        </div>
+                    </div>
+                    <div className='jumbotron2-content-2-box2'>
+                        <div className='jumbotron2-content-2-box2-logo'>
+                            <img src="https://cf.shopee.co.id/file/e9dcf5cda94694077a72e350917688b9_tn" alt="" width='100%' height='100%'/>
+                        </div>
+                        <div className='jumbotron2-content-2-box2-title'>
+                            Aksesoris & Fashion
+                        </div>
+                    </div>
+                    <div className='jumbotron2-content-2-box2'>
+                        <div className='jumbotron2-content-2-box2-logo'>
+                            <img src="https://cf.shopee.co.id/file/23ae7eeda2ceedd08a6b6559328cbd4f_tn" alt="" width='100%' height='100%'/>
+                        </div>
+                        <div className='jumbotron2-content-2-box2-title'>
+                            Photography
+                        </div>
+                    </div>
+                    <div className='jumbotron2-content-2-box2'>
+                        <div className='jumbotron2-content-2-box2-logo'>
+                            <img src="https://cf.shopee.co.id/file/b1d4a1b303e074db103b613cc63d383a_tn" alt="" width='100%' height='100%'/>
+                        </div>
+                        <div className='jumbotron2-content-2-box2-title'>
+                            Otomotif
+                        </div>
+                    </div>
+                    <div className='jumbotron2-content-2-box2'>
+                        <div className='jumbotron2-content-2-box2-logo'>
+                            <img src="https://cf.shopee.co.id/file/369fb7e53c325c89057c4388709a4023_tn" alt="" width='100%' height='100%'/>
+                        </div>
+                        <div className='jumbotron2-content-2-box2-title'>
+                            Voucher
+                        </div>
+                    </div>
+                    <div className='jumbotron2-content-2-box2'>
+                        <div className='jumbotron2-content-2-box2-logo'>
+                            <img src="https://cf.shopee.co.id/file/810fcc29f815c84548c7c57bbd614b42_tn" alt="" width='100%' height='100%'/>
+                        </div>
+                        <div className='jumbotron2-content-2-box2-title'>
+                            Souvenir & Pesta
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
